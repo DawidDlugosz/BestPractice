e5fb0ac0994ab930da30a3ef7b170a65c4de16f0
Dawid Dlugosz <dawid.dlugosz@gmail.com>
2019-09-10 23:59:08 +0200

    Permission Api

---
+++ src/PermissionApi/02-Caching/CacheConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using RedisCaching;
+
+namespace CacheConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var redisCache = new RedisCachManager();
+            redisCache.StartRedis();
+            redisCache.StartRedis();
+            redisCache.StartRedis();
+            redisCache.StartRedis();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/PermissionApi/02-Caching/RedisCaching/RedisCachManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisCaching
+{
+    public class RedisCachManager
+    {
+        public void StartRedis()
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost");
+            var db = redis.GetDatabase();
+            db.StringSet("aaa", "aa");
+        }
+    }
+}
